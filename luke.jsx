2236207cfd3c5a379b643bf8f060dd454cdb89a1
CELSO ARON PEREIRA SILVA LEME <celso.apsleme@senacsp.edu.br>
2023-08-24 20:40:05 -0300

    api fake

---
+++ luke.jsx
@@ -0,0 +1,22 @@
+export function getLukes(){
+    return[
+        {
+         "title": "Calça CArgo Beje",
+         "image":"https://cdn.dooca.store/3005/products/dsc-1418-1_450x600+fill_ffffff+crop_center.jpg?v=1676668396",
+          "price": 238.98,
+          "url": "https://www.lojaverse.com.br/calca-verse-jogger-cargo"
+        },
+        {
+            "title": "Calça CArgo Beje",
+            "image":"https://cdn.dooca.store/3005/products/dsc-1418-1_450x600+fill_ffffff+crop_center.jpg?v=1676668396",
+             "price": 238.98,
+             "url": "https://www.lojaverse.com.br/calca-verse-jogger-cargo",
+           },
+           {
+            "title": "Calça CArgo Beje",
+            "image":"https://cdn.dooca.store/3005/products/dsc-1418-1_450x600+fill_ffffff+crop_center.jpg?v=1676668396",
+             "price": 238.98,
+             "url": "https://www.lojaverse.com.br/calca-verse-jogger-cargo"
+           }
+    ]
+}
